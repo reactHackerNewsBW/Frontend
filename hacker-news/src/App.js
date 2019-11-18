5b67aef3f7bfefc201753fd49bd92a5c325eee2f
Darrin Lowery <lambda@monsto.com>
2019-11-18 14:15:45 -0600

    login scaffolded.

--- hacker-news/src/App.js
+++ hacker-news/src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Route,
+	Link,
+	Switch
+} from "react-router-dom";
+// import Login from "./components/Login"
+
+import Login from "./components/tempLog";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+	return (
+		
+		<p><Login /></p>
+		// <Router>
+		// 	<div className="App">
+		// 		<header className="App-header">
+		// 			<p><Link to="/login">LOGIN</Link></p>
+		// 		</header>
+		// 	</div>
+		// 	<Route path="/login" component={Login} />
+		// </Router>
+	);
 }
 
 export default App;
