864da61a3f9f40263f18009258464aba848cdf41
AbdelIdir <homeworkoracle@protonmail.com>
2019-11-18 23:37:29 +0100

    Register page done

--- hacker-news/src/App.js
+++ hacker-news/src/App.js
@@ -1,30 +1,23 @@
 import React from "react";
-import {
-	BrowserRouter as Router,
-	Route,
-	Link,
-	Switch
-} from "react-router-dom";
-// import Login from "./components/Login"
-
+import "./App.css";
+import {Register} from "./Register";
 import Login from "./components/tempLog";
-import Register from "./components/tempReg";
+// import Register from "./components/tempReg";
 import List from "./components/tempList";
-import './App.css';
+
+// import styled from "styled-components";
+
+
+
+
 
 function App() {
-	return (
-		<Router>
-			<div className="App">
-				<p><Link to="/login">Login to existing</Link></p>
-				<p><Link to="/register">Register new</Link></p>
-			</div>
-			<Route exact path="/login" component={Login} />
-			<Route exact path="/register" component={Register} />
-			<Route exact path="/list" component={List} />
-			{/* <Route render={() => { return 404 }} /> */}
-		</Router>
-	);
+  return (
+    <div className="App">
+      
+<Register/>
+    </div>
+  );
 }
 
 export default App;
---
+++ hacker-news/src/Register.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Form, Icon, Input, Button } from "antd";
+import "antd/dist/antd.css";
+import styled from "styled-components";
+
+export const Register = () => {
+  const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 700px;
+    padding-top: 22%;
+  `;
+  const DivForForm = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 500px;
+    margin: 0 auto;
+    align-items: center;
+    justify-content: center;
+  `;
+
+  return (
+    <Container>
+      <DivForForm>
+        <Form style={{ width: "400px" }}>
+          <Form.Item>
+            <Input
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Input
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Email"
+              type="email"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Input
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Password"
+              type="password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              style={{ color: "white", background: "#0dbd10" }}
+            >
+              Register
+            </Button>
+          </Form.Item>
+        </Form>
+      </DivForForm>
+    </Container>
+  );
+};
