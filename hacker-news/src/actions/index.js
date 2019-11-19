ed854b44c235e2392c1e942bfdfb33a6374c2360
Darrin Lowery <lambda@monsto.com>
2019-11-19 14:30:40 -0600

    starting on redux

---
+++ hacker-news/src/actions/index.js
@@ -0,0 +1,12 @@
+export const REG_START = "REG_START";
+export const REG_SUCCESS = "REG_SUCCESS";
+export const REG_FAILED = "REG_FAILED";
+
+export const LOGIN_START = "REG_START";
+export const LOGIN_SUCCESS = "REG_SUCCESS";
+export const LOGIN_FAILED = "REG_FAILED";
+
+function clg(...x) {
+	for (let exes of x) console.log(exes);
+}
+
--- hacker-news/src/index.js
+++ hacker-news/src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
+import reducer from "./reducers"
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from "react-router-dom";
+// import * as serviceWorker from './serviceWorker';
 
-import * as serviceWorker from './serviceWorker';
+const store = createStore(reducer, applyMiddleware(thunk, logger))
 
 ReactDOM.render(
-	<Router>
-		<App />
-	</Router>
-	, document.getElementById('root'));
+	<Provider store={store}>
+		<Router>
+			<App />
+		</Router>
+	</Provider>
+	, document.getElementById('root')
+);
