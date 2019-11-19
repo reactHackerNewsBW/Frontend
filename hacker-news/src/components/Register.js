bd52b19eb21f50fa8fb5af35e265155e23063871
AbdelIdir <homeworkoracle@protonmail.com>
2019-11-19 21:25:16 +0100

    Refactored the code for the form on Register.js : Took outt styled components, and used vanilla CSS instead. Now using react bootstrap instead of antDesign. -Finished making the list on ListItem.js

--- hacker-news/src/components/Register.js
+++ hacker-news/src/components/Register.js
@@ -1,104 +1,104 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Form, Icon, Input, Button } from "antd";
-import "antd/dist/antd.css";
-import styled from "styled-components";
-
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function clg(...exes) {
-	for (let x of exes) console.log(x);
+  for (let x of exes) console.log(x);
 }
 
-const URL = "http://localhost:5000/api"
+const URL = "http://localhost:5000/api";
 
 export const Register = () => {
-	const [register, setRegister] = useState({ username: "", password: "", email:"" });
-	const [isRegged, setIsRegged] = useState(false);
+  const [register, setRegister] = useState({
+    username: "",
+    password: "",
+    email: ""
+  });
+  const [isRegged, setIsRegged] = useState(false);
 
-	//#region styled
-	const Container = styled.div`
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		height: 700px;
-		padding-top: 22%;
-	`;
-	const DivForForm = styled.div`
-		display: flex;
-		flex-direction: column;
-		width: 500px;
-		margin: 0 auto;
-		align-items: center;
-		justify-content: center;
-	`;
-	//#endregion styled
+  //#endregion styled
 
-	// control form fields
-	const doChange = e => {
-		clg(e.target.name, e.target.value) 
-		setRegister({ ...register, [e.target.name]: e.target.value });
-	}
+  // control form fields
+  const doChange = e => {
+    clg(e.target.name, e.target.value);
+    setRegister({ ...register, [e.target.name]: e.target.value });
+  };
 
-	// form submit
-	const registerAction = e => {
-		e.preventDefault();
-		// axios call to get register from backend
-		axios
+  // form submit
+  const registerAction = e => {
+    e.preventDefault();
+    // axios call to get register from backend
+    axios
 
-			/*	
+      /*	
 			WATCH FOR CHANGE
 			*/
-			.post(`${URL}/register`, register)
-			.then(res => {
-
-				/*
+      .post(`${URL}/register`, register)
+      .then(res => {
+        /*
 				WATCH FOR CHANGE
 				*/
-				sessionStorage.setItem("token", res.data.payload);
+        sessionStorage.setItem("token", res.data.payload);
 
-				setIsRegged(true)
-				clg("--- Registered ++")
-				// props.history.push("list");
-			})
-			.catch(err => clg(`>>> PROBLEM -- Register > axios :: ${err}`))
-	}
+        setIsRegged(true);
+        clg("--- Registered ++");
+        // props.history.push("list");
+      })
+      .catch(err => clg(`>>> PROBLEM -- Register > axios :: ${err}`));
+  };
 
+  return (
+    <div className="Container">
+      <Form onSubmit={registerAction}>
+        <Form.Group controlId="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter username"
+            name="username"
+            value={register.username}
+            onChange={doChange}
+          />
+        </Form.Group>
 
-	return (
-		<Container>
-			<DivForForm>
-				<Form onSubmit={registerAction} style={{ width: "400px" }} >
-					<Form.Item>
-						<Input
-							name="username" value={register.username} onChange={doChange}
-							prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
-							placeholder="Username" type="text"
-						/>
-					</Form.Item>
-					<Form.Item>
-						<Input
-							name="email" value={register.email} onChange={doChange}
-							prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
-							placeholder="Email" type="email"
-						/>
-					</Form.Item>
-					<Form.Item>
-						<Input
-							name="password" value={register.password} onChange={doChange}
-							prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
-							placeholder="Password" type="password"
-						/>
-					</Form.Item>
-					<Form.Item>
-						<Button
-							type="primary"
-							htmlType="submit"
-							style={{ color: "white", background: "#0dbd10" }}
-						>Register</Button>
-					</Form.Item>
-				</Form>
-			</DivForForm>
-		</Container>
-	);
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            value={register.email}
+            onChange={doChange}
+          />
+          <Form.Text
+            className="text-muted"
+            style={{
+              color: "black",
+              background: "white",
+              textDecoration: "underline"
+            }}
+          >
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
 
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={register.password}
+            onChange={doChange}
+          />
+        </Form.Group>
+
+        <Button variant="info" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 };
