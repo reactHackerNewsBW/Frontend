07fedc53163ef86eedad7bebc8addae62f673e9d
Darrin Lowery <lambda@monsto.com>
2019-11-20 10:23:49 -0600

    wed morn line updates

--- hacker-news/src/components/Register.js
+++ hacker-news/src/components/Register.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { connect } from "react-redux";
 import { register } from "../actions"
 
